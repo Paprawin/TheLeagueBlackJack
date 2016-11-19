caa05658079b7135f0396d1fe444e9de4f193d93
Paprawin Boonyakida <paprawin@hotmail.com>
2016-11-19 02:23:03 -0500

    Added Player.java
    Deleted numPlayers
    Deleted Dealer and Hands slasses
    Deleted UID
    Removed splitting function
    Renamed getLegalActions to getAvailableActions

---
+++ src/theleagueblackjack/main.java
@@ -0,0 +1,24 @@
+/*
+Group: Edsel Rudy, Giancarlo Soriano, Jasmine Santos, Paprawin Boonyakida
+
+
+NOTE for MySql: user: root   password: password
+
+
+ */
+package theleagueblackjack;
+
+public class main{
+    
+    public static void main(String[] args) {
+        
+        //Test Player
+        Player player1 = new Player();
+        
+        gameDatabase database = new gameDatabase();
+        
+        
+        
+    }
+    
+}
